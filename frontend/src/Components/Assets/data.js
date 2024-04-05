bc6f6271c138736fb44ddc7e3cc0b065899b975c
Pragnesh271 <pragneshrathwa2701@gmail.com>
2024-04-05 17:34:06 +0530

    Initial commit

---
+++ frontend/src/Components/Assets/data.js
@@ -0,0 +1,34 @@
+import p1_img from './product_1.png'
+import p2_img from './product_2.png'
+import p3_img from './product_3.png'
+import p4_img from './product_4.png'
+
+let data_product = [
+  {
+    id:1,
+    name:"Apple Watch",
+    image:p1_img,
+    new_price:15000.00,
+    old_price:20000.00,
+  },
+  {id:2,
+    name:"Playstation 4",
+    image:p2_img,
+    new_price:29000.00,
+    old_price:45000.00,
+  },
+  {id:3,
+    name:"Adidas 1X SERIES ",
+    image:p3_img,
+    new_price:950.00,
+    old_price:1000.00,
+  },
+  {id:4,
+    name:"Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse",
+    image:p4_img,
+    new_price:100.00,
+    old_price:150.00,
+  },
+];
+
+export default data_product;
